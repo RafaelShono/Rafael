56856e47af3d50884f3a717e15024439d74adf1b
Rafael <rafael.ifpr.2019@gmail.com>
2020-05-28 20:10:36 -0300

    Iniciando o respositório

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
+    <link rel="stylesheet" href="css/style.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo List</title>
+</head>
+<body>
+    <header>
+      
+    </header>
+        <div class="container">
+            <div id="procon">
+            <h1 id="srap">SRAP</h1>
+        </div>
+        <div id="pesquisa-user">
+            <input id="pesquisa" type="search" placeholder="Digite nome ou CPF/ CNPJ"><button id="enviar">Pesquisar</button>
+            <button id="novo-cadastro">Novo cadastro</button>
+        </div>
+        <div id="info-atendimento">
+            <table>
+                <tr id="info">
+                    <th class="td">Nome</th>
+                    <th class="td">CPF/CNPJ</th>
+                    <th class="td">Data de Atendimento</th>
+                    <th class="td-descricao">Descrição</th>
+             </tr>
+                <tr class="resultado">
+                <td>Rafael Shono</td>
+                <td>094.855.959-48</td>
+                <td>22/05/2020</td>
+                <td class="descricao">Reclamação contra a loja Salfer devido a preços abusivos</td>
+            </tr>
+            <tr>
+                <td>Rafael Shono</td>
+                <td>094.855.959-48</td>
+                <td>22/05/2020</td>
+                <td class="descricao">Reclamação contra a loja Salfer devido a preços abusivos</td>
+            </tr>
+            <tr>
+                <td>Rafael Shono</td>
+                <td>094.855.959-48</td>
+                <td>22/05/2020</td>
+                <td class="descricao">Reclamação contra a loja Salfer devido a preços abusivos</td>
+            </tr>
+            </table>
+        </div>
+    </div>
+
+    
+    <scrit src="app.js">
+    </scrit>
+</body>
+</html>
